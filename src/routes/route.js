51bc8ee9c0294079373e0dfea7281c91e5f959d5
Satyasarode <satyasnarayana6@gmail.com>
2022-06-05 09:58:30 +0530

    welcome to css

--- src/routes/route.js
+++ src/routes/route.js
@@ -46,12 +46,48 @@ console.log(lodash.union(num1,num2,num3,num4,num5))
 });
 
 
+  router.get("/sol1", function (req, res) {
+    //logic : sum of numbers is n(n+1)/2..so get sum of all numbers in array. now take sum of numbers till last digit in the array
+    let arr= [1,2,3,5,6,7]
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+  
+    let lastDigit= arr.pop()
+    let consecutiveSum= lastDigit * (lastDigit+1) / 2
+    let missingNumber= consecutiveSum - total
+  
+    res.send(  { data: missingNumber  }  );
+  });
+ 
+  router.get("/sol2", function (req, res) {
+    //logic : sum of n consecutive numbers is [ n * (first + last) / 2  ]..so get sum of all numbers in array. now take sum of n consecutive numbers.. n would be length+1 as 1 number is missing
+    let arr= [33, 34, 35, 37, 38]
+    let len= arr.length
+  
+    let total = 0;
+    for (var i in arr) {
+        total += arr[i];
+    }
+  
+    let firstDigit= arr[0]
+    let lastDigit= arr.pop()
+    let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+    let missingNumber= consecutiveSum - total
+   
+    res.send(  { data: missingNumber  }  );
+  });
+ 
+
 router.get('/test-me1', function (req, res) {
-    res.send('My second ever api!')
+
+    res.send({a:5,b:7})
 });
 
-router.get('/test-me2', function (req, res) {
-    res.send('My third api!')
+router.get('/test-post', function (req, res) {
+    res.send([1,2,3])
 });
 
 router.get('/test-me3', function (req, res) {
